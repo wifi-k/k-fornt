b62f8de63fe1f85952f189e98ee31df57b1af6ca
chenv <wei.chen@treebear.cn>
2019-01-10 14:52:29 +0800

    delete

--- opens/static/js/7.798aff47c6b928eec07a.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([7],{496:function(e,n,r){var t=r(202)(null,r(550),null,null,null);e.exports=t.exports},550:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("router-view")},staticRenderFns:[]}}});
