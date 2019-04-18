9927762a553daaa8b9df656a7bde286b250be450
chenv <wei.chen@treebear.cn>
2019-04-18 16:57:25 +0800

    delete

--- open/src/components/management/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Administrator on 2017/6/5.
- */
-module.exports = {
-    personal: resolve => require(['./personal/personal.vue'],resolve),
-}
--- open/src/store/router/actions.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../mutation-types';
-
-export default {
-    set_header_route: ({
-        commit
-    }, paths) => {
-        commit(types.SET_CUR_ROUTE, paths);
-    }
-};
